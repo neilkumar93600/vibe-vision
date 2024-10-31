b1ae500e5264223e759197c2ae3f5530f560a6c6
Nilesh kumar <71492461+neilkumar93600@users.noreply.github.com>
2024-10-31 19:33:07 +0530

    Delete app/(auth) directory

--- app/(auth)/page.tsx
+++
@@ -1,8 +0,0 @@
-// app/page.tsx
-import dynamic from 'next/dynamic'
-
-const HomePage = dynamic(() => import('./home/page'), { ssr: false })
-
-export default function Page() {
-  return <HomePage />
-}
